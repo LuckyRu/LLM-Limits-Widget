39fc6b1c552b92d46436cdab9ef8044a2c1b5809
Alexander Leshchenko <a.leshchenko@xdevs.ltd>
2026-08-13 20:55:53 +0300

    fix: tighten widget vertical spacing

--- spikes/FloatingOverlay/WidgetAppearance.cs
+++ spikes/FloatingOverlay/WidgetAppearance.cs
@@ -3,9 +3,9 @@ namespace LLMLimitsWidget.FloatingOverlay;
 public sealed class WidgetAppearance
 {
     public double VerticalWidth { get; init; } = 285;
-    public double VerticalHeight { get; init; } = 112;
+    public double VerticalHeight { get; init; } = 103;
     public double HorizontalWidth { get; set; } = 545;
-    public double HorizontalHeight { get; init; } = 64;
+    public double HorizontalHeight { get; init; } = 55;
     public double Scale { get; set; } = 1.0;
     public double SurfaceOpacity { get; set; } = 1.0;
     public double CornerRadius { get; set; } = 18;
